579664b14b4f2029e3dbc39f0aeaf37fb7ed3d2b
Divya Mereddy <divyamereddy9@gmail.com>
2017-08-01 11:29:09 -0400

    Initial Commit

---
+++ Symptom Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Symptom Tracker
+//
+//  Created by Divya Mereddy on 8/1/17.
+//  Copyright © 2017 Divya Mereddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
